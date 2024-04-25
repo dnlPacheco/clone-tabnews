b610fca57c872e68cd954d7a79b05564a49da2fa
Daniel <dnl.lucas@gmail.com>
2024-04-25 21:13:42 +0000

    add new text to Home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Título :)</h1>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Olá, família!</h1>
+      <h3>site em construção...</h3>
+    </div>
+  );
 }
 
 export default Home;
